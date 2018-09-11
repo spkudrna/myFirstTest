1095916101133c99283c242329195ebd77196c0e
Sean Kudrna <spkudrna@stu.naperville203.org>
2018-09-11 12:19:14 -0500

    Finished Parking

--- AnotherTest/Parking.java
+++ AnotherTest/Parking.java
@@ -5,25 +5,40 @@ public class Parking
     public static void main(String[] args)
     {
        Scanner scan = new Scanner(System.in);
-       Stack driveway = new Stack();
-       Stack street = new Stack();
+       Stack <Integer> driveway = new Stack<>();
+       Stack <Integer> street = new Stack<>();
        
        Integer car= 1;
        while (car != 0)
        {
-           System.out.println("Enter licence plate to add a car (- to remove): ");
+           System.out.println("\nEnter licence plate to add a car (- to remove): ");
            car = scan.nextInt();
            
-           Integer compare = 0;
+
+           
+           
+           //add
+           if (car.intValue() > 0)
+           {
+               System.out.print(driveway.push(car) + " Has been added to driveway\n");
+           }
            
            //Remove
-           if (car < compare)
+           else if (car.intValue() < 0)
            {
-               while (driveway.peek() != car)
+               while (driveway.peek().intValue() != Math.abs(car.intValue()))
+              
                {
-                   street.push(driveway.pop());
+                   System.out.print(street.push(driveway.pop()) + " Has been moved out of driveway to street\n");;
                }
-               driveway.pop();
+               System.out.println(driveway.pop() + " Has been located and removed\n");
+               
+               //adds street cars back into driveway
+               System.out.println();
+               for (Integer cars : street)
+               {
+                   System.out.println(driveway.push(cars) + " Has been added back to driveway");
+               }
            }
        }
     }
